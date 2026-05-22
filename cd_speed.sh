ac41f41390f42dc4d69d504bb17f885bf18085f8
Charlie Porto <cportodev@gmail.com>
2026-05-21 20:35:29 -0500

    clean top level script

--- cd_speed.sh
+++ cd_speed.sh
@@ -2,20 +2,9 @@
 
 main()
 {
-    #!< TODO: update
     local cds_backend="/usr/local/bin/cd-speed"
 
-    # TODO: implement ability to CRUD dr keywords (*including nested)
-    local flags_map=(
-        -a --add
-        -rm --remove
-        -rl --relink
-    )
-    # If no args passed to backend, it prints the manual and extts
-    if [[ -z "$1" ]]; then
-        $cds_backend
-        return
-    fi
+    # If no args passed, print the manual and exit
     local result="$($cds_backend "$@")"
     if [[ "$result" == cds:* ]]; then
         local dest_dir="${result#*:}"
